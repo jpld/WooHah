f819cfd776d326fba02f7a656552c60582ee6079
jpld <imjunpyo@gmail.com>
2010-05-19 21:15:06 -0700

    add skeleton for option parsing

--- woohah.rb
+++ woohah.rb
@@ -2,3 +2,57 @@
 #
 #  Created by jpld on 19 May 2010.
 #  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+
+require 'open3'
+require 'ostruct'
+require 'optparse'
+require 'fileutils'
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+
+WOOHAH_VERSION = "0.0.0"
+
+CHECKER_PAGE_URI = 'http://clang-analyzer.llvm.org/'
+
+# settings to be tweaked by the user
+CHECKER_INSTALL_LOCATION = '~/bin/'
+CHECKER_SYMLINK_LOCATION = '~/bin/checker'
+CLEAN = true
+
+
+
+if $0 == __FILE__
+  options = OpenStruct.new
+  opts = OptionParser.new do |opts|
+    opts.banner = "Usage: #{File.basename(__FILE__, File.extname(__FILE__))} [options]"
+
+    opts.separator ""
+    opts.separator "Common options:"
+
+    opts.on_tail("-h", "--help", "Show this message") do
+      puts opts
+      exit
+    end
+
+    # Another typical switch to print the version.
+    opts.on_tail('-v', "--version", "Show version") do
+      puts WOOHAH_VERSION
+      exit
+    end
+  end
+
+
+  # unknown args throw an exception
+  begin
+    opts.parse! ARGV
+  rescue OptionParser::ParseError => e
+    puts e
+    puts opts
+    exit 1
+  end
+
+  # dump help for now
+  puts opts
+  exit    
+end
